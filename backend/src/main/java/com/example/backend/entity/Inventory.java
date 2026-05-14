01cdcf46134c2d982eca196066ac956af02021d5
navam142 <navamsharma142@gmail.com>
2026-05-14 11:19:20 +0530

    inventory entity created

---
+++ backend/src/main/java/com/example/backend/entity/Inventory.java
@@ -0,0 +1,30 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import com.example.backend.entity.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inventory_id")
+    private Long inventoryId;
+
+    @Column(name = "film_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "film_id")
+    private Film filmId;
+
+    @Column(name = "store_id")
+    private Long storeId;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
+}
