61bb5c3b1b98a7091bf4c9388a3b54b4366b5df6
Akanksha Chopra <chopraakanksha10@gmail.com>
2026-05-14 15:45:37 +0530

    import

--- backend/src/test/java/com/example/backend/repository/ActorRepositoryTest.java
+++ backend/src/test/java/com/example/backend/repository/ActorRepositoryTest.java
@@ -1,7 +1,6 @@
-package com.example.backend.Repository;
+package com.example.backend.repository;
 
 import com.example.backend.entity.Actor;
-import com.example.backend.repository.ActorRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
