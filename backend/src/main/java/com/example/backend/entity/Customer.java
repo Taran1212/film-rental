02bc36b2a26ecfbac29d5ac54036f77d595dd9fe
vishalraj096 <vishal20031singh@gmail.com>
2026-05-15 14:59:32 +0530

    fixing variable name

--- backend/src/main/java/com/example/backend/entity/Customer.java
+++ backend/src/main/java/com/example/backend/entity/Customer.java
@@ -32,7 +32,7 @@ public class Customer {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "address_id", nullable = false)
-    private Address addressId;
+    private Address address;
 
     @Column(name = "active")
     private Boolean active;
