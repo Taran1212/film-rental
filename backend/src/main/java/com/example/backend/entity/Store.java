cca253331d337178bae5eb8f479878dc75f4f106
Ashish Yadav <ashish@Ashishs-MacBook-Air.local>
2026-05-15 14:58:26 +0530

    Mapping store entity

--- backend/src/main/java/com/example/backend/entity/Store.java
+++ backend/src/main/java/com/example/backend/entity/Store.java
@@ -1,10 +1,12 @@
 package com.example.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "store")
@@ -16,12 +18,31 @@ public class Store {
     @Column(name = "store_id")
     private Integer storeId;
 
-    @Column(name = "manager_staff_id")
-    private Integer managerStaffId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_staff_id", insertable = false, updatable = false)
+    @JsonIgnore
+    private Staff managerStaff;
 
     @Column(name = "address_id")
     private Integer addressId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id", insertable = false, updatable = false)
+    @JsonIgnore
+    private Address address;
+
     @Column(name = "last_update")
     private LocalDateTime lastUpdate;
+
+    @OneToMany(mappedBy = "store", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Staff> staffMembers;
+
+    @OneToMany(mappedBy = "store", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Customer> customers;
+
+    @OneToMany(mappedBy = "store", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Inventory> inventories;
 }
