6ae8156ecadc21da6c13ca08f984b24f1c0609ad
diljot <diljotkamboj2811@gmail.com>
2026-05-16 18:01:28 +0530

    added rentalrequest and rentalconfirmation Dto

---
+++ backend/src/main/java/com/example/backend/dto/RentalConfirmationDto.java
@@ -0,0 +1,23 @@
+package com.movie_rental_backend.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+public class RentalConfirmationDto {
+
+    private Integer rentalId;
+
+    private String movieTitle;
+
+    private String customerName;
+
+    private Integer copyId;
+
+    private BigDecimal amount;
+}
