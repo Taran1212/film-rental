7376cd43c8bf81721d644c685299803c9e8c2080
Akanksha Chopra <chopraakanksha10@gmail.com>
2026-05-16 20:19:03 +0530

    city country and rental repo methods with projections

--- backend/src/main/java/com/example/backend/repository/CityRepository.java
+++ backend/src/main/java/com/example/backend/repository/CityRepository.java
@@ -1,12 +1,16 @@
 package com.example.backend.repository;
 
+import com.example.backend.dto.projection.CityProjection;
 import com.example.backend.entity.City;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CityRepository extends JpaRepository<City,Integer> {
 
     Page<City> findAll(Pageable pageable);
 
+    List<CityProjection> findByCountryIdOrderByCityAsc(Integer countryId);
 }
